8d335c49058ed1b93856184dac6c2ad7698265ed
MiyajiTakenari <100999489+MiyajiTakenari@users.noreply.github.com>
2024-06-17 17:26:29 +0900

    main

---
+++ euler_subr.f90
@@ -0,0 +1,25 @@
+program euler_2D_A1
+    use params
+    use globals
+    use interface_mod
+
+    implicit none
+    integer :: n
+    call init
+    !時間進める
+    do n = 1, nmax
+        call bound
+        call cflc
+        if (exit_flag == 1) exit
+        call xflux
+        call nextq
+        call calc_res
+        call writed(n)
+    end do
+
+    write(*, '(a9, i4)') "n_time =", n - 1
+    write(*, *) "res_rho =", res(1)
+    write(*, *) "res_u   =", res(2)
+    write(*, *) "res_p   =", res(3)
+
+end program euler_2D_A1
