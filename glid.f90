331ed0b8202581a07a2a1f916123de4bc7bfc2bf
MiyajiTakenari <100999489+MiyajiTakenari@users.noreply.github.com>
2024-06-19 08:30:08 +0900

    glid仮

---
+++ glid.f90
@@ -0,0 +1,13 @@
+subroutine glid
+    use params
+    use globals
+
+    implicit none
+    integer i, j
+    do i = 0, imax
+        do j = 0, jmax
+            
+        end do
+    end do
+
+end subroutine glid
