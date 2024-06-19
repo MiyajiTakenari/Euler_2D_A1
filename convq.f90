d0738a94bf42cba478929a55ac80bcb258f8953c
MiyajiTakenari <100999489+MiyajiTakenari@users.noreply.github.com>
2024-06-19 20:13:51 +0900

    4変数実装

--- convq.f90
+++ convq.f90
@@ -1,13 +1,14 @@
-function qtobq(rho, u, p) result(big_q)
+function qtobq(rho, u, v, p) result(big_q)
     use params
     use globals
 
     implicit none
-    real(8), intent(in) :: rho, u, p
-    real(8) big_q(3)
+    real(8), intent(in) :: rho, u, v, p
+    real(8) big_q(4)
     big_q(1) = rho
     big_q(2) = rho * u
-    big_q(3) = (p/(gamma-1.0d0)) + 0.5d0 * rho * u * u
+    big_q(3) = rho * v
+    big_q(4) = (p/(gamma-1.0d0)) + 0.5d0 * (rho * u * u + rho * v * v)
 end function qtobq
 
 function bqtoq(big_q) result(q)
@@ -15,9 +16,10 @@ function bqtoq(big_q) result(q)
     use globals
 
     implicit none
-    real(8), intent(in) :: big_q(3)
-    real(8) q(3)
+    real(8), intent(in) :: big_q(4)
+    real(8) q(4)
     q(1) = big_q(1)
     q(2) = big_q(2)/big_q(1)
-    q(3) = (gamma-1.0d0) * (big_q(3) - 0.5d0 * big_q(1) * (big_q(2)/big_q(1)) ** 2.0d0)
+    q(3) = big_q(3)/big_q(1)
+    q(4) = (gamma-1.0d0) * (big_q(4) - 0.5d0 * big_q(1) * ((big_q(2)/big_q(1)) ** 2.0d0 + (big_q(3)/big_q(1)) ** 2.0d0))
 end function bqtoq
