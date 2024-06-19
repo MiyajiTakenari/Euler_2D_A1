1d1f7c72ba0002fea544110e474fddab00bfebd6
MiyajiTakenari <100999489+MiyajiTakenari@users.noreply.github.com>
2024-06-19 11:43:26 +0900

    glid_物理空間

--- glid.f90
+++ glid.f90
@@ -4,9 +4,13 @@ subroutine glid
 
     implicit none
     integer i, j
+    allocate(x(0:imax, 0:jmax), y(0:imax, 0:jmax))
     do i = 0, imax
         do j = 0, jmax
-            
+            !x(i, j) = dble(i)
+            !y(i, j) = dble(j)
+            x(i, j) = -0.5d0 + dx * dble(i - 1)
+            y(i, j) = -0.5d0 + dy * dble(i - 1)
         end do
     end do
 
