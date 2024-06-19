63d011210bc62dbfe4bc5c7768008ada9b6de977
MiyajiTakenari <100999489+MiyajiTakenari@users.noreply.github.com>
2024-06-19 20:13:27 +0900

    globals割り付け

---
+++ allocate.f90
@@ -0,0 +1,18 @@
+subroutine alloc(al_flag)
+    use params
+    use globals
+
+    implicit none
+    integer, intent(in) :: al_flag
+    if (al_flag == 1) then
+        allocate (x(-4:imax+2, -4:jmax+2), y(-4:imax+2, -4:jmax+2))
+        allocate (bq(-2:imax+2, -2:jmax+2, 4))
+        allocate (e(-1:imax, -1:jmax, 4))
+        allocate (mx(-3:imax+2, -3:jmax+2), my(-3:imax+2, -3:jmax+2))
+        allocate (nx(-3:imax+2, -3:jmax+2), ny(-3:imax+2, -3:jmax+2))
+        allocate (s_j(0:imax, 0:jmax))
+    else if (al_flag == 0) then
+        deallocate (x, y, bq, e, mx, my, nx, ny, s_j)
+    end if
+
+end subroutine alloc
