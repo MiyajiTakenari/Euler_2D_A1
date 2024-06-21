4a54a73e783c79a5005c47965b2f28b5017d32ac
MiyajiTakenari <100999489+MiyajiTakenari@users.noreply.github.com>
2024-06-21 20:38:41 +0900

    コメント追加

--- allocate.f90
+++ allocate.f90
@@ -10,6 +10,7 @@ subroutine alloc(al_flag)
         allocate (e(-1:imax, -1:jmax, 4))
         allocate (mx(-3:imax+2, -3:jmax+2), my(-3:imax+2, -3:jmax+2))
         allocate (nx(-3:imax+2, -3:jmax+2), ny(-3:imax+2, -3:jmax+2))
+        !B.Cのfuncion ave_n, ave_mでmetricsの配列の下限値使ってる。下限値変更時注意
         allocate (s_j(0:imax, 0:jmax))
     else if (al_flag == 0) then
         deallocate (x, y, bq, e, mx, my, nx, ny, s_j)
