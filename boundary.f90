c26f835075f21c2a10e9018623f2853961311098
MiyajiTakenari <100999489+MiyajiTakenari@users.noreply.github.com>
2024-06-24 18:39:14 +0900

    BD1実装

--- boundary.f90
+++ boundary.f90
@@ -67,6 +67,7 @@ subroutine bound
     !slip condition
     !j+1=j, u_j+1 = -u_j, rho_j+1 = rho_j, e_j+1 = e_j
     do i = -2, 102
+        !101=100, u_101 = -u_100, rho_101 = rho_100, e_101 = e_100
         !rho_101 = rho_100
         !temp_q(2) = u_100 = u, temp_q(3) = v_100 = v
         temp_q(:) = bqtoq(bq(i, 100, :))
@@ -82,6 +83,7 @@ subroutine bound
         bq(i, 101, :) = qtobq(q_bc(1), q_bc(2), q_bc(3), 0.0d0)
         bq(i, 101, 4) = bq(i, 100, 4)
 
+        !102=99, u_102 = -u_99, rho_102 = rho_99, e_102 = e_99
         !rho_102 = rho_99
         !temp_q(2) = u_99 = u, temp_q(3) = v_99 = v
         temp_q(:) = bqtoq(bq(i, 99, :))
@@ -96,6 +98,43 @@ subroutine bound
         !pは適当、e=bq(i, 102, 4)はe_102 = e_99
         bq(i, 102, :) = qtobq(q_bc(1), q_bc(2), q_bc(3), 0.0d0)
         bq(i, 102, 4) = bq(i, 99, 4)
+    end do
+
+    !BD1ひな型
+    !slip condition
+    !i=i+1, u_i = -u_i+1, uu_i = uu_i+1, rho_i = rho_i+1, e_i = e_i+1
+    do j = -2, 102
+        !-1=0, u_-1 = -u_0, uu_-1 = uu_0, rho_-1 = rho_0, e_-1 = e_0
+        !rho_-1 = rho_0
+        !temp_q(2) = u_0 = u, temp_q(3) = v_0 = v
+        temp_q(:) = bqtoq(bq(0, j, :))
+        q_bc(1) = temp_q(1)
+        u = temp_q(2)
+        v = temp_q(3)
+        ! bu=U_0, bv=UU_0を求め、q_bc(2)=u_-1, q_bc(3)=v_-1を求める
+        bu = ave_n(nx, 0, j) * u + ave_n(ny, 0, j) * v
+        bv = -ave_n(ny, 0, j) * u + ave_n(nx, 0, j) * v
+        q_bc(2) = (-ave_n(nx, -1, j) * bu - ave_n(ny, -1, j) * bv) / (ave_n(nx, -1, j) ** 2.0d0 + ave_n(ny, -1, j) ** 2.0d0)
+        q_bc(3) = (-ave_n(ny, -1, j) * bu + ave_n(nx, -1, j) * bv) / (ave_n(nx, -1, j) ** 2.0d0 + ave_n(ny, -1, j) ** 2.0d0)
+        !pは適当、e=bq(-1, j, 4)はe_-1 = e_0
+        bq(-1, j, :) = qtobq(q_bc(1), q_bc(2), q_bc(3), 0.0d0)
+        bq(-1, j, 4) = bq(0, j, 4)
+
+        !-2=1, u_-2 = -u_1, uu_-2 = uu_1, rho_-2 = rho_1, e_-2 = e_1
+        !rho_-2 = rho_1
+        !temp_q(2) = u_1 = u, temp_q(3) = v_1 = v
+        temp_q(:) = bqtoq(bq(1, j, :))
+        q_bc(1) = temp_q(1)
+        u = temp_q(2)
+        v = temp_q(3)
+        ! bu=U_1, bv=UU_1を求め、q_bc(2)=u_-2, q_bc(3)=v_-2を求める
+        bu = ave_n(nx, 1, j) * u + ave_n(ny, 1, j) * v
+        bv = -ave_n(ny, 1, j) * u + ave_n(nx, 1, j) * v
+        q_bc(2) = (-ave_n(nx, -2, j) * bu - ave_n(ny, -2, j) * bv) / (ave_n(nx, -2, j) ** 2.0d0 + ave_n(ny, -2, j) ** 2.0d0)
+        q_bc(3) = (-ave_n(ny, -2, j) * bu + ave_n(nx, -2, j) * bv) / (ave_n(nx, -2, j) ** 2.0d0 + ave_n(ny, -2, j) ** 2.0d0)
+        !pは適当、e=bq(-2, j, 4)はe_-2 = e_1
+        bq(-2, j, :) = qtobq(q_bc(1), q_bc(2), q_bc(3), 0.0d0)
+        bq(-2, j, 4) = bq(1, j, 4)
     end do
 
 end subroutine bound
@@ -140,4 +179,25 @@ end subroutine bound
     !!pは適当、e=bq(i, j+1, 4)はe_j+1 = e_j
     !bq(i, j+1, :) = qtobq(q_bc(1), q_bc(2), q_bc(3), 0.0d0)
     !bq(i, j+1, 4) = bq(i, j, 4)
+!end do
+
+!BD1ひな型
+!!slip condition
+!!i=i+1, u_i = -u_i+1, uu_i = uu_i+1, rho_i = rho_i+1, e_i = e_i+1
+!do j = -2, 102
+    !!rho_i = rho_i+1
+    !!temp_q(2) = u_i+1 = u, temp_q(3) = v_i+1 = v
+    !temp_q(:) = bqtoq(bq(i+1, j, :))
+    !q_bc(1) = temp_q(1)
+    !u = temp_q(2)
+    !v = temp_q(3)
+    !! bu=U_i+1, bv=UU_i+1を求め、q_bc(2)=u_i, q_bc(3)=v_iを求める
+    !bu = ave_n(nx, i+1, j) * u + ave_n(ny, i+1, j) * v
+    !bv = -ave_n(ny, i+1, j) * u + ave_n(nx, i+1, j) * v
+    !q_bc(2) = (-ave_n(nx, i, j) * bu - ave_n(ny, i, j) * bv) / (ave_n(nx, i, j) ** 2.0d0 + ave_n(ny, i, j) ** 2.0d0)
+    !q_bc(3) = (-ave_n(ny, i, j) * bu + ave_n(nx, i, j) * bv) / (ave_n(nx, i, j) ** 2.0d0 + ave_n(ny, i, j) ** 2.0d0)
+
+    !!pは適当、e=bq(i, j, 4)はe_i = e_i+1
+    !bq(i, j, :) = qtobq(q_bc(1), q_bc(2), q_bc(3), 0.0d0)
+    !bq(i, j, 4) = bq(i+1, j, 4)
 !end do
