410d270d9aa8a8e78af6f78666a2beebaaff0e34
MiyajiTakenari <100999489+MiyajiTakenari@users.noreply.github.com>
2024-06-25 15:46:09 +0900

    コメント追加

--- parameters_mod.f90
+++ parameters_mod.f90
@@ -4,5 +4,5 @@ module params
     real(8), parameter :: range_x = 1.0d0, dx = range_x / (dble(imax+1) - 1.0d0), &
     & range_y = 1.0d0, dy = range_x / (dble(jmax+1) - 1.0d0), &
     ex_time = 0.2d0, cfl = 0.15d0, gamma = 1.4d0, beta = 2.5d0, phi = 1.0d0 / 3.0d0
-    !dx注意：range_x/(xの配列数-1)
+    !dx,dy注意：range_x/(xの配列数-1)
 end module params
