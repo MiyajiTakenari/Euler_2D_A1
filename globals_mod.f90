4d43d4630a463545f8b677459da5305f6544e12d
MiyajiTakenari <100999489+MiyajiTakenari@users.noreply.github.com>
2024-06-25 19:57:06 +0900

    res_x,y実装

--- globals_mod.f90
+++ globals_mod.f90
@@ -3,6 +3,4 @@ module globals
     real(8), allocatable, save :: x(:, :), y(:, :), bq(:, :, :), e(:, :, :), f(:, :, :), &
     & mx(:, :), my(:, :), nx(:, :), ny(:, :), s_j(:, :), res_x(:, :), res_y(:, :)
     integer, save :: exit_flag = 0
-    ! x(-4:imax+2, -4:jmax+2), y(-4:imax+2, -4:jmax+2)
-    ! bq(-2:imax+2, -2:jmax+2, 4), e(-1:imax, -1:jmax, 4)
 end module globals
