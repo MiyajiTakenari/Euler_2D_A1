ecb53e3d11e7ac2ee3e258cf660c088ab499e7a2
MiyajiTakenari <100999489+MiyajiTakenari@users.noreply.github.com>
2024-06-25 21:53:02 +0900

    j=50をwrite

--- write_data.f90
+++ write_data.f90
@@ -5,18 +5,19 @@ subroutine writed(n)
 
     implicit none
     integer, intent(in) :: n
-    integer j
-    real(8) temp_q(3)
+    integer i, j
+    real(8) temp_q(4)
     open(10, file = 'data_q.csv')
-    do j = 0, jmax
-        temp_q(:) = bqtoq(bq(j, 1:3))
-        write(10, *) x(j), ',', temp_q(1), ',', temp_q(2), ',', temp_q(3)
+    j = 50
+    do i = 0, imax
+        temp_q(:) = bqtoq(bq(i, j, :))
+        write(10, *) x(i, j), ',', temp_q(1), ',', temp_q(2), ',', temp_q(3), ',', temp_q(4)
     enddo
     close(10)
 
     open(11, file = 'data_tres.csv')
     write(11, '(a9, a1, f12.10, a1, a9, a1, i4)') "time =", ',', time, ',', "n_time =", ',', n
-    write(11, '(a9, a1, a9, a1, a9, a1, a9)') "x", ',', "rho", ',', "u", ',', "p"
-    write(11, '(a9, a1, e12.6, a1, e12.6, a1, e12.6)') "res", ',', res(1), ',', res(2), ',', res(3)
+    write(11, '(a9, a1, a9, a1, a9, a1, a9, a1, a9)') "x", ',', "rho", ',', "rho*u", ',', "rho*v", ',', "e"
+    write(11, '(a9, a1, e12.6, a1, e12.6, a1, e12.6, a1, e12.6)') "res", ',', res_x(j, 1), ',', res_x(j, 2), ',', res_x(j, 3), ',', res_x(j, 4)
     close(11)
 end subroutine writed
