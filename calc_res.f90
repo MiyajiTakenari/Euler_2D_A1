54ca7471374dadf29e2c15de6cf3d07af540ceda
MiyajiTakenari <100999489+MiyajiTakenari@users.noreply.github.com>
2024-06-25 20:33:04 +0900

    res実装

--- calc_res.f90
+++ calc_res.f90
@@ -3,10 +3,22 @@ subroutine calc_res
     use globals
 
     implicit none
-    integer j
-    res(1:3) = 0.0d0
+    integer i, j
+    !res_xはx方向の残差,それがy本(0:jmax)ある
+    !res_yはy方向の残差,それがx本(0:imax)ある
+    res_x(:, :) = 0.0d0
+    res_y(:, :) = 0.0d0
     do j = 0, jmax
-        res(:) = res(:) + abs(-(dt / dx) * (e(j, :) - e(j-1, :))) / (jmax+1)
-        !xの配列数で割る
-    enddo
+        do i = 0, imax
+            res_x(j, :) = res_x(j, :) + abs(bq(i, j, :) - bq_n(i, j, :)) / dble(imax + 1)
+            res_y(i, :) = res_y(i, :) + abs(bq(i, j, :) - bq_n(i, j, :)) / dble(jmax + 1)
+        end do
+    end do
+
+    !do i = 0, imax
+        !do j = 0, jmax
+            !res_y(i, :) = res_y(i, :) + abs(bq(i, j, :) - bq_n(i, j, :)) / dble(jmax + 1)
+        !end do
+    !end do
+
 end subroutine calc_res
