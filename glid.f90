39300c970a21a65d9190b1f2925092e1deb6651f
MiyajiTakenari <100999489+MiyajiTakenari@users.noreply.github.com>
2024-09-02 10:02:49 +0900

    -0.5 + dx * i

--- glid.f90
+++ glid.f90
@@ -6,11 +6,9 @@ subroutine glid
     integer i, j
     do i = -4, imax+2
         do j = -4, jmax+2
-            x(i, j) = dble(i)
-            y(i, j) = dble(j)
             !物理空間初期分布
-            !x(i, j) = -0.5d0 + dx * dble(i)
-            !y(i, j) = -0.5d0 + dy * dble(j)
+            x(i, j) = -0.5d0 + dx * dble(i)
+            y(i, j) = -0.5d0 + dy * dble(j)
         end do
     end do
 
