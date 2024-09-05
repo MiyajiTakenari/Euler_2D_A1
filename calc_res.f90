454d6f9d61169eaf4739f7f70372393b19792e71
MiyajiTakenari <100999489+MiyajiTakenari@users.noreply.github.com>
2024-09-06 04:05:36 +0900

    min実装、配列数変更

--- calc_res.f90
+++ calc_res.f90
@@ -4,14 +4,14 @@ subroutine calc_res
 
     implicit none
     integer i, j
-    !res_xはx方向の残差,それがy本(0:jmax)ある
-    !res_yはy方向の残差,それがx本(0:imax)ある
+    !res_xはx方向の残差,それがy本(jmin:jmax)ある
+    !res_yはy方向の残差,それがx本(imin:imax)ある
     res_x(:, :) = 0.0d0
     res_y(:, :) = 0.0d0
-    do j = 0, jmax
-        do i = 0, imax
-            res_x(j, :) = res_x(j, :) + abs(bq(i, j, :) - bq_n(i, j, :)) / dble(imax + 1)
-            res_y(i, :) = res_y(i, :) + abs(bq(i, j, :) - bq_n(i, j, :)) / dble(jmax + 1)
+    do j = jmin, jmax
+        do i = imin, imax
+            res_x(j, :) = res_x(j, :) + abs(bq(i, j, :) - bq_n(i, j, :)) / dble(imax - imin + 1)
+            res_y(i, :) = res_y(i, :) + abs(bq(i, j, :) - bq_n(i, j, :)) / dble(jmax - jmin + 1)
         end do
     end do
 
