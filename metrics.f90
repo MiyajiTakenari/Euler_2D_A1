2e74773ea2fdcf6516fcfebef9a9a7b4b746e787
MiyajiTakenari <100999489+MiyajiTakenari@users.noreply.github.com>
2024-09-06 02:38:53 +0900

    min実装、配列数変更 imin,jminを実装、Q:0~100を1~100に変更

--- metrics.f90
+++ metrics.f90
@@ -4,17 +4,23 @@ subroutine metrics
 
     implicit none
     integer i, j
-    do i = -3, imax+2
-        do j = -3, jmax+2
+
+    do i = imin-3, imax+2
+        do j = jmin-2, jmax+2
             mx(i, j) = (y(i, j) - y(i, j-1))
             my(i, j) = -(x(i, j) - x(i, j-1))
+        end do
+    end do
+
+    do j = jmin-3, jmax+2
+        do i = imin-2, imax+2
             nx(i, j) = -(y(i, j) - y(i-1, j))
             ny(i, j) = (x(i, j) - x(i-1, j))
         end do
     end do
 
-    do i = 0, imax
-        do j = 0, jmax
+    do i = imin, imax
+        do j = jmin, jmax
             s_j(i, j) = ny(i, j) * mx(i, j) - nx(i, j) * my(i, j)
         end do
     end do
