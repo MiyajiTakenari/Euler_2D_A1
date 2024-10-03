f5fe7d4752b61452e5ab1fa4fe4a44799f3cd6ef
MiyajiTakenari <100999489+MiyajiTakenari@users.noreply.github.com>
2024-10-03 21:44:03 +0900

    call glid 削除

--- initial.f90
+++ initial.f90
@@ -1,13 +1,13 @@
 subroutine init
     use params
     use globals
-    use interface_mod, only : qtobq, glid
+    use interface_mod, only : qtobq
 
     implicit none
     integer i, j
 
     !初期条件Q
-    call glid
+    !call glid
     do i = imin-2, imax+2
         do j = jmin-2, jmax+2
             if (x(i, j) <= 0.0d0) then
